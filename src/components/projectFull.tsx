753dc7e0a05abbf34da05bcda4ac559cc55a7572
chrstinalin <44586776+chrstinalin@users.noreply.github.com>
2023-09-14 12:26:04 -0400

    scrollbar

--- src/components/projectFull.tsx
+++ src/components/projectFull.tsx
@@ -7,7 +7,7 @@ const ProjectFull = (props: { banner: any, logo: any; title: any; description: a
 
     return (
         <React.Fragment>
-            <div onClick={(e) => {e.stopPropagation()}} className={"h-full w-full bg-white z-50 rounded-lg border-b border-gray-300 bg-gradient-to-b from-zinc-200  dark:border-neutral-800 dark:bg-zinc-900 dark:from-inherit"}>
+            <div onClick={(e) => {e.stopPropagation()}} className={"overflow-hidden h-full w-full bg-white z-50 rounded-lg border-b border-gray-300 bg-gradient-to-b from-zinc-200  dark:border-neutral-800 dark:bg-zinc-900 dark:from-inherit"}>
                 <div className={"h-80 overflow-hidden"}>
                     <img src={banner} alt="header" className="w-full brightness-75 z-0" />
                 </div>
