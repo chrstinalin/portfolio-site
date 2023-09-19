ad696a11dd00e0a1450c6ddc57d19c99f7b00441
chrstinalin <44586776+chrstinalin@users.noreply.github.com>
2023-09-18 22:20:01 -0400

    tag div

--- src/components/tag/allTagSections.tsx
+++ src/components/tag/allTagSections.tsx
@@ -5,7 +5,7 @@ import { FrameworkIcon, LanguageIcon, ToolIcon } from "../decorative/icon";
 
 export const AllTagSections = () => {
     return (
-        <div className="w-screen flex flex-wrap items-center justify-center gap-2 md:m-12">
+        <div className="w-screen flex flex-wrap items-center justify-center md:m-12">
             <TagSection Icon={<LanguageIcon />} key={"Languages"} title={"Languages"} tags={INFO.languages} />
             <TagSection Icon={<FrameworkIcon />} key={"Frameworks"} title={"Frameworks"} tags={INFO.frameworks} />
             <TagSection Icon={<ToolIcon />} key={"Tools"} title={"Tools"} tags={INFO.tools} />
--- src/components/tag/tagSection.tsx
+++ src/components/tag/tagSection.tsx
@@ -5,7 +5,7 @@ export const TagSection = (props: { title: any; tags: any; Icon: any}) => {
 
     return (
         <React.Fragment>
-            <div className={"rounded-lg p-6 w-56 h-44 flex flex-col items-center gap-2"}>
+            <div className={"rounded-lg w-56 h-44 flex flex-col items-center gap-1"}>
                 <p className="h-8 text-l font-bold flex gap-2">{Icon}{title}</p>
                 <div className={"flex flex-wrap w-48 justify-center items-center gap-1"}>
                     {tags.map((tag: string) => <div className={"px-2 rounded-full border-b border-gray-300 backdrop-blur-2xl border-neutral-800 bg-zinc-900 from-inherit"} key={tag}>{tag}</div>)}
